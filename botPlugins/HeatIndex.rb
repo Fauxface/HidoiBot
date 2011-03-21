a6b49765bc16b97b47ab84cc50e8fa2cf6cc4c30
Guoyou Ng <ng.guoyou@gmail.com>
2011-03-22 05:52:37 +0800

    fixed error for real

--- botPlugins/HeatIndex.rb
+++ botPlugins/HeatIndex.rb
@@ -11,7 +11,7 @@
     def main(data)
         tempC = arguments(data)[0]
         humidity = arguments(data)[1]
-        return "say '#{calcHeatIndex(tempC, humidity)}'"
+        return "say '#{calcHeatIndex(tempC.to_f, humidity.to_f)}'"
     rescue => e
         handleError(e)
         return nil
@@ -22,6 +22,6 @@
 		con1, con2, con3, con4, con5, con6, con7, con8, con9 = -42.379, 2.04901523, 10.14333127, -0.22475541, -6.83783 * 10**-3, -5.481717 * 10**-2, 1.22874 * 10**-3, 8.5282 * 10**-4, -1.99 * 10**-6
 		hiF = con1 + con2 * tempF + con3 * humidity + con4 * tempF * humidity + con5 * tempF**2 + con6 * humidity**2 + con7 * tempF**2 * humidity + con8 * tempF * humidity**2 + con9 * tempF**2 * humidity**2
 		hiC = (hiF - 32) * 5 / 9
-		return hiC
+		return "Heat Index: #{hiC.to_s[0..5]}"
 	end
 end
