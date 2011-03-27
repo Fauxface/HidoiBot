a448feb581976c5c469d87b4602e93816beb6cc7
Guoyou Ng <ng.guoyou@gmail.com>
2011-03-27 19:34:39 +0800

    move reminder into plugin

--- core/Timer.rb
+++ core/Timer.rb
@@ -77,6 +77,10 @@ module Timer
     end
     
     def addEvent(user, type, time, occurrence, occurrenceOffset, *message)
+        if time.class != Time
+            time = Time.at(time.to_i)
+        end
+    
         event = {
             # Who to remind
             "user" => user,
