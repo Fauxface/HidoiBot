7c63eef356e45eca1111e7790a610cf061f0bf06
Guoyou Ng <ng.guoyou@gmail.com>
2011-06-18 20:02:12 +0800

    Made console commands work more than once - welp

--- HidoiBot2.rb
+++ HidoiBot2.rb
@@ -137,20 +137,22 @@ def consoleInput
     # To use this, type '/<$botName.method>' in the console
     if @console != true
         Thread.new do
-            @console = true
-            consoleInput = nil
-
-            while consoleInput == nil
-                consoleInput = STDIN.gets.chomp
+            loop do
+                @console = true
+                consoleInput = nil
+                
+                while consoleInput == nil
+                    consoleInput = STDIN.gets.chomp
+                end
+                
+                if consoleInput[0] == '/'
+                    consoleInput[0] = ''
+                    puts "Evaluating console input: #{consoleInput}"
+                    eval(consoleInput)
+                end
+                
+                consoleInput = nil
             end
-            
-            if consoleInput[0] == '/'
-                consoleInput[0] = ''
-                puts "Evaluating console input: #{consoleInput}"
-                eval(consoleInput)
-            end
-            
-            consoleInput = nil
         end
     end
 end
