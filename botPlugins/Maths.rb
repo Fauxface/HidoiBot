aecadc226c39e50a94382e147181a76c7d9b547a
Guoyou Ng <ng.guoyou@gmail.com>
2011-09-07 03:46:40 +0800

    Cleanups

--- botPlugins/Maths.rb
+++ botPlugins/Maths.rb
@@ -70,80 +70,80 @@ class Maths < BotPlugin
     return outputQueue
   end
 
-    def reversePolishNotation(expr)
-      # According to http://en.wikipedia.org/wiki/Reverse_polish_notation#Postfix_algorithm
-      stack = []
+  def reversePolishNotation(expr)
+    # According to http://en.wikipedia.org/wiki/Reverse_polish_notation#Postfix_algorithm
+    stack = []
 
-      while expr.size > 0
-        if isNumber?(expr.first)
-          stack.push(expr.shift.to_f)
-        else
-          # If fewer values than function/operator's arguments
-          raise 'Syntax Error: Insufficient values' if stack.size < 2
-
-          rightVal = stack.pop
-          leftVal = stack.pop
-          stack.push(leftVal.send(expr.first, rightVal))
-          expr.shift
-        end
-
-          puts "RPN: Stack: #{stack.join(" ")} Expression: #{expr.join(" ")}"
-      end
-
-      return stack[0]
-    end
-
-    def getPrecedence(c)
-      case c
-      when '^' then
-        return {p: 4, a: 'right'}
-      when '**' then
-        return {p: 4, a: 'right'}
-      when '*' then
-        return {p: 3, a: 'left'}
-      when '/' then
-        return {p: 3, a: 'left'}
-      when '+' then
-        return {p: 2, a: 'left'}
-      when '-' then
-        return {p: 2, a: 'left'}
-      when '%' then
-        return {p: 3, a: 'left'}
+    while expr.size > 0
+      if isNumber?(expr.first)
+        stack.push(expr.shift.to_f)
       else
-        return {p: 0, a: 'left'}
+        # If fewer values than function/operator's arguments
+        raise 'Syntax Error: Insufficient values' if stack.size < 2
+
+        rightVal = stack.pop
+        leftVal = stack.pop
+        stack.push(leftVal.send(expr.first, rightVal))
+        expr.shift
       end
+
+        puts "RPN: Stack: #{stack.join(" ")} Expression: #{expr.join(" ")}"
     end
 
-    def isOperator?(c)
-      if /(\^|\*\*|\*|\/|\+|\-|\%)/ === c
-        return true
-      else
-        return false
-      end
-    end
+    return stack[0]
+  end
 
-    def isNumber?(s)
-      if /\d+(.\d+)?/ === s
-        return true
-      else
-        return false
-      end
+  def getPrecedence(c)
+    case c
+    when '^' then
+      return {p: 4, a: 'right'}
+    when '**' then
+      return {p: 4, a: 'right'}
+    when '*' then
+      return {p: 3, a: 'left'}
+    when '/' then
+      return {p: 3, a: 'left'}
+    when '+' then
+      return {p: 2, a: 'left'}
+    when '-' then
+      return {p: 2, a: 'left'}
+    when '%' then
+      return {p: 3, a: 'left'}
+    else
+      return {p: 0, a: 'left'}
     end
+  end
 
-    def formatInput(s)
-      # Can be improved
-      s.gsub!(/ /, '')
-      s.gsub!('*', ' * ')
-      s.gsub!('^', ' ** ')
-      s.gsub!(' *  * ', ' ** ')
-      s.gsub!('+', ' + ')
-      s.gsub!('-', ' - ')
-      s.gsub!('/', ' / ')
-      s.gsub!('%', ' % ')
-      s.gsub!('(', ' ( ')
-      s.gsub!(')', ' ) ')
-      s = s.split(' ')
-
-      return s
+  def isOperator?(c)
+    if /(\^|\*\*|\*|\/|\+|\-|\%)/ === c
+      return true
+    else
+      return false
     end
+  end
+
+  def isNumber?(s)
+    if /\d+(.\d+)?/ === s
+      return true
+    else
+      return false
+    end
+  end
+
+  def formatInput(s)
+    # Can be improved
+    s.gsub!(/ /, '')
+    s.gsub!('*', ' * ')
+    s.gsub!('^', ' ** ')
+    s.gsub!(' *  * ', ' ** ')
+    s.gsub!('+', ' + ')
+    s.gsub!('-', ' - ')
+    s.gsub!('/', ' / ')
+    s.gsub!('%', ' % ')
+    s.gsub!('(', ' ( ')
+    s.gsub!(')', ' ) ')
+    s = s.split(' ')
+
+    return s
+  end
 end
