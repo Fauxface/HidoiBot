427721d44031e57519175b53bff2ae3b698194e8
Guoyou Ng <ng.guoyou@gmail.com>
2011-09-07 01:47:46 +0800

    Cleanups

--- botPlugins/Maths.rb
+++ botPlugins/Maths.rb
@@ -1,149 +1,147 @@
-﻿class Maths < BotPlugin
-    def initialize
-        # Authorisations
-        @reqAuthLevel = 0
+﻿# Ng Guoyou
+# Maths.rb
+# This plugin evaluates basic mathematical expressions without the use of eval,
+# through the judacious usage of shunting-yard algorithm and reverse polish notation.
 
-        # Strings
-        @noAuthMessage = "You are not authorised for this."
+class Maths < BotPlugin
+  def initialize
+    # Authorisations
+    @reqAuthLevel = 0
 
-        # Required plugin stuff
-        name = self.class.name
-        @hook = ["math", "maths"]
-        processEvery = false
-        help = "Usage: #{@hook} <term>\nFunction: Evaluates basic mathematical expressions. Accepts +, -, /, *, ^, ** and % as operators."
-        super(name, @hook, processEvery, help)
-    end
+    # Strings
+    @noAuthMessage = "You are not authorised for this."
 
-    def main(data)
-        @givenLevel = data["authLevel"]
+    # Required plugin stuff
+    name = self.class.name
+    @hook = ["math", "maths"]
+    processEvery = false
+    help = "Usage: #{@hook} <term>\nFunction: Evaluates basic mathematical expressions. Accepts +, -, /, *, ^, ** and % as operators."
+    super(name, @hook, processEvery, help)
+  end
 
-        if authCheck(@reqAuthLevel)
-            return sayf(reversePolishNotation(shuntInput(formatInput(stripWordsFromStart(data["message"], 1)))))
-        else
-            return sayf(@noAuthMessage)
-        end
-    rescue => e
-        handleError(e)
-        return sayf(e)
-    end
+  def main(data)
+    @givenLevel = data["authLevel"]
 
-    def shuntInput(s)
-        # According to http://en.wikipedia.org/wiki/Shunting-yard_algorithm#The_algorithm_in_detail
-        stack = []
-        outputQueue = []
+    return authCheck(@reqAuthLevel) ? sayf(reversePolishNotation(shuntInput(formatInput(stripWordsFromStart(data["message"], 1))))) : sayf(@noAuthMessage)
+  rescue => e
+    handleError(e)
+    return sayf(e)
+  end
 
-        s.each { |token|
-            if  isNumber?(token)
-                outputQueue.push(token)
-            elsif isOperator?(token)
-                while isOperator?(stack.last) && 
-                        (
-                            (getPrecedence(token)[:a] == 'left' && (getPrecedence(token)[:p] <= getPrecedence(stack.last)[:p])) ||
-                            (getPrecedence(token)[:a] == 'right' && (getPrecedence(token)[:p] < getPrecedence(stack.last)[:p]))
-                        )
-                    outputQueue.push(stack.pop)
-                end
-                stack.push(token)
-            elsif token == '('
-                stack.push(token)
-            elsif token == ')'
-                while stack.last != '('
-                    outputQueue.push(stack.pop)
-                    
-                    if stack.size == 0
-                        raise "Syntax Error: Mismatched parentheses"
-                    end
-                end
-                
-                stack.pop if stack.last == '('
-            end
-        }
+  def shuntInput(s)
+    # According to http://en.wikipedia.org/wiki/Shunting-yard_algorithm#The_algorithm_in_detail
+    stack = []
+    outputQueue = []
 
-        while stack.size > 0
-            if stack.last == '('
-                raise "Syntax Error: Mismatched parentheses"
-            else
-                outputQueue.push(stack.pop)
-            end
+    s.each { |token|
+      if isNumber?(token)
+        outputQueue.push(token)
+      elsif isOperator?(token)
+        while isOperator?(stack.last) && 
+          ((getPrecedence(token)[:a] == 'left' && (getPrecedence(token)[:p] <= getPrecedence(stack.last)[:p])) ||
+           (getPrecedence(token)[:a] == 'right' && (getPrecedence(token)[:p] < getPrecedence(stack.last)[:p])))
+          outputQueue.push(stack.pop)
         end
 
-        puts "Shunted: #{outputQueue.join(' ')}"
-        return outputQueue
+        stack.push(token)
+      elsif token == '('
+        stack.push(token)
+      elsif token == ')'
+        while stack.last != '('
+          outputQueue.push(stack.pop)
+          raise "Syntax Error: Mismatched parentheses" if stack.size == 0
+        end
+
+        stack.pop if stack.last == '('
+      end
+    }
+
+    while stack.size > 0
+      if stack.last == '('
+        raise "Syntax Error: Mismatched parentheses"
+      else
+        outputQueue.push(stack.pop)
+      end
     end
+
+    puts "Shunted: #{outputQueue.join(' ')}"
+    return outputQueue
+  end
 
     def reversePolishNotation(expr)
-        # According to http://en.wikipedia.org/wiki/Reverse_polish_notation#Postfix_algorithm
-        stack = []
+      # According to http://en.wikipedia.org/wiki/Reverse_polish_notation#Postfix_algorithm
+      stack = []
 
-        while expr.size > 0
-            if isNumber?(expr.first)
-                stack.push(expr.shift.to_f)
-            else
-                # If fewer values than function/operator's arguments
-                raise 'Syntax Error: Insufficient values' if stack.size < 2
+      while expr.size > 0
+        if isNumber?(expr.first)
+          stack.push(expr.shift.to_f)
+        else
+          # If fewer values than function/operator's arguments
+          raise 'Syntax Error: Insufficient values' if stack.size < 2
 
-                rightVal = stack.pop
-                leftVal = stack.pop
-                stack.push(leftVal.send(expr.first, rightVal))
-                expr.shift
-            end
-            
-            puts "RPN: Stack: #{stack.join(" ")} Expression: #{expr.join(" ")}"
+          rightVal = stack.pop
+          leftVal = stack.pop
+          stack.push(leftVal.send(expr.first, rightVal))
+          expr.shift
         end
-        
-        return stack[0]
+
+          puts "RPN: Stack: #{stack.join(" ")} Expression: #{expr.join(" ")}"
+      end
+
+      return stack[0]
     end
 
     def getPrecedence(c)
-        case c
-        when '^' then
-            return {p: 4, a: 'right'}
-        when '**' then
-            return {p: 4, a: 'right'}
-        when '*' then
-            return {p: 3, a: 'left'}
-        when '/' then
-            return {p: 3, a: 'left'}
-        when '+' then
-            return {p: 2, a: 'left'}
-        when '-' then
-            return {p: 2, a: 'left'}
-        when '%' then
-            return {p: 3, a: 'left'}
-        else
-            return {p: 0, a: 'left'}
-        end
+      case c
+      when '^' then
+        return {p: 4, a: 'right'}
+      when '**' then
+        return {p: 4, a: 'right'}
+      when '*' then
+        return {p: 3, a: 'left'}
+      when '/' then
+        return {p: 3, a: 'left'}
+      when '+' then
+        return {p: 2, a: 'left'}
+      when '-' then
+        return {p: 2, a: 'left'}
+      when '%' then
+        return {p: 3, a: 'left'}
+      else
+        return {p: 0, a: 'left'}
+      end
     end
 
     def isOperator?(c)
-        if /(\^|\*\*|\*|\/|\+|\-|\%)/ === c
-            return true
-        else
-            return false
-        end
+      if /(\^|\*\*|\*|\/|\+|\-|\%)/ === c
+        return true
+      else
+        return false
+      end
     end
 
     def isNumber?(s)
-        if /\d+(.\d+)?/ === s
-            return true
-        else
-            return false
-        end
+      if /\d+(.\d+)?/ === s
+        return true
+      else
+        return false
+      end
     end
 
     def formatInput(s)
-        s.gsub!(/ /, '')
-        s.gsub!('*', ' * ')
-        s.gsub!('^', ' ** ')
-        s.gsub!(' *  * ', ' ** ')
-        s.gsub!('+', ' + ')
-        s.gsub!('-', ' - ')
-        s.gsub!('/', ' / ')
-        s.gsub!('%', ' % ')
-        s.gsub!('(', ' ( ')
-        s.gsub!(')', ' ) ')
-        s = s.split(' ')
-        
-        return s
+      # Can be improved
+      s.gsub!(/ /, '')
+      s.gsub!('*', ' * ')
+      s.gsub!('^', ' ** ')
+      s.gsub!(' *  * ', ' ** ')
+      s.gsub!('+', ' + ')
+      s.gsub!('-', ' - ')
+      s.gsub!('/', ' / ')
+      s.gsub!('%', ' % ')
+      s.gsub!('(', ' ( ')
+      s.gsub!(')', ' ) ')
+      s = s.split(' ')
+
+      return s
     end
 end
