cd9522100f2bf9e4edd34c62cf63835281e42880
Guoyou Ng <ng.guoyou@gmail.com>
2012-06-08 16:42:24 +0800

    Entitle plugin

---
+++ botPlugins/Entitle.rb
@@ -0,0 +1,129 @@
+﻿# Ng Guoyou
+# Entitle.rb
+# (Automatically) gets titles of URLs, with filters.
+
+class Entitle < BotPlugin
+  def initialize
+    require 'nokogiri' # Gem
+    require 'open-uri'
+
+    # Default Persistent Settings
+    @s = {
+      'active' => true,
+      'timeout' => 10,
+      'trackedSites' => ['\b.*youtube.com.*\b', '\b.*www.google.com.*\b']
+    }
+
+    @settingsFile = "entitle/settings.json"
+    loadSettings
+
+    # Authorisations
+    @reqAdminAuth = 3
+    @reqQueryAuth = 0
+
+    # Strings
+    @noModeMsg = "An invalid mode was given."
+    @noSuchFilterMsg = "The given filter is currently not tracked."
+
+    # Required plugin stuff
+    name = self.class.name
+    @hook = "entitle"
+    processEvery = true
+    help = "Usage: #{@hook} (query|on|off|status|filters|add|remove) <term>\nFunction: Returns titles of URLs which match a predefined filter. Filters are regular expressions."
+    super(name, @hook, processEvery, help)
+  end
+
+  def main(m)
+    if m.processEvery && @s['active']
+      reply = checkFilter(m)
+    elsif !m.processEvery
+      case m.mode
+      when 'on'
+        @s['active'] = true if m.authR(@reqAdminAuth)
+        saveSettings
+        m.reply(getStatus)
+
+      when 'off'
+        @s['active'] = false if m.authR(@reqAdminAuth)
+        saveSettings
+        m.reply(getStatus)
+
+      when 'status'
+        m.reply(getStatus) if m.authR(@reqQueryAuth)
+
+      when 'filters'
+        m.reply(getFilters) if m.authR(@reqAdminAuth)
+
+      when 'add'
+        if m.authR(@reqAdminAuth)
+          addFilter(m) if m.authR(@reqAdminAuth)
+          m.reply(getStatus)
+        end
+
+      when 'remove'
+        removeFilter(m) if m.authR(@reqAdminAuth)
+        m.reply(getStatus)
+
+      when 'query'
+        m.reply(getTitle(m.args[1])) if m.authR(@reqQueryAuth)
+
+      else
+        m.reply(@noModeMsg)
+      end
+    end
+
+    return nil
+  rescue => e
+    handleError(e)
+    return nil
+  end
+
+  def checkFilter(m)
+    tempMessage = String.new(m.message)
+
+    @s['trackedSites'].each { |regex|
+      results = tempMessage.scan(Regexp.new(regex))
+
+      if !results.empty?
+        results.each { |result|
+          Thread.new do
+            timeout(@s["timeout"]) do
+              title = getTitle(result)
+              m.reply(title) if title != nil
+            end
+          end
+
+          # Remove matched URL to prevent double-counting
+          tempMessage.gsub!(result)
+        }
+      end
+    }
+  end
+
+  def getTitle(url)
+    puts "Entitle: Getting title of #{url}"
+    return Nokogiri::HTML(open(url)).at_css("title").text.gsub(/ *\n */, " ").lstrip.rstrip
+  end
+
+  def getStatus
+    return "Entitle is #{@s['active'] ? 'active' : 'inactive'}. Tracking #{@s['trackedSites'].size} filters."
+  end
+
+  def getFilters
+    return @s["trackedSites"].join(", ")
+  end
+
+  def addFilter(m)
+    @s['trackedSites'].push(m.args[1]) if !@s['trackedSites'].include?(m.args[1])
+    saveSettings
+  end
+
+  def removeFilter(m)
+    if @s['trackedSites'].include?(m.args[1])
+      @s['trackedSites'].delete(m.args[1])
+      saveSettings
+    else
+      m.reply(@noSuchFilterMsg)
+    end
+  end
+end
